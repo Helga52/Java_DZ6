df5dbca0d912136afeab23d92ff46ad58762a9ef
Olga <helga_ser@mail.ru>
2023-08-05 16:52:37 +0300

    ParamTest

--- src/test/java/ru/netology/servises/CountOfRestTest.java
+++ src/test/java/ru/netology/servises/CountOfRestTest.java
@@ -2,36 +2,14 @@ package ru.netology.servises;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 
 public class CountOfRestTest {
-
-    @Test
-    void shouldCalculateCountOfRest1() {
+    @ParameterizedTest
+    @CsvFileSource(files="src/test/resources/countofrest.csv")
+        void shouldCalculateCountOfRest(int expected,int income, int expenses, int threshold) {
         CountOfRest service = new CountOfRest();
-
-        // подготавливаем данные
-        int income = 10_000;
-        int expenses = 3_000;
-        int threshold = 20_000;
-        int expected = 3;
-
-        // вызываем целевой метод:
-        int actual = service.calculate(income, expenses, threshold);
-
-        // производим проверку (сравниваем ожидаемый и фактический):
-        Assertions.assertEquals(expected, actual);
-
-    }
-
-    @Test
-    void shouldCalculateCountOfRest2() {
-        CountOfRest service = new CountOfRest();
-
-        // подготавливаем данные
-        int income = 100_000;
-        int expenses = 60_000;
-        int threshold = 150_000;
-        int expected = 2;
 
         // вызываем целевой метод:
         int actual = service.calculate(income, expenses, threshold);
